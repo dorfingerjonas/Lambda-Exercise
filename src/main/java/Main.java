5dd2e53aab73ae26a1ca6fc03cc6930edf93ee84
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-01-10 10:50:09 +0100

    finished task2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        final String path = "pupils.csv";
 
+        try {
+
+            List<Pupil> pupils = new ArrayList<>();
+
+            Files.readAllLines(Paths.get(path)).stream()
+                    .skip(1)
+                    .peek(System.out::println)
+                    .map(line -> line.split(";"))
+                    .map(elements -> new Pupil(Integer.parseInt(elements[0]), elements[1], elements[2], elements[3], elements[4], elements[5]))
+                    .peek(pupils::add)
+                    .forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
